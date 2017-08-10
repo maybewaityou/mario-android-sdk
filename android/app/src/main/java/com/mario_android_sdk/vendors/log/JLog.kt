113701ea7d20aacc596d0dcf6c7e74a04fbf45e0
MeePwn <maybewaityou@gmail.com>
2017-08-11 06:09:08 +0800

    first commit

--- android/app/src/main/java/com/mario_android_sdk/vendors/log/JLog.kt
+++ android/app/src/main/java/com/mario_android_sdk/vendors/log/JLog.kt
@@ -5,12 +5,6 @@ import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import com.orhanobut.logger.PrettyFormatStrategy
 
-
-
-
-
-
-
 /**
  * Created by MeePwn on 2017/8/10.
  */
